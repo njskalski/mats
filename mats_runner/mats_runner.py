c7c64ab49a25584f9459cddc38ce488760476621
Andrzej Skalski <askalski>
2012-03-30 13:29:53 +0200

    updated readme

--- mats_runner/mats_runner.py
+++ mats_runner/mats_runner.py
@@ -2,18 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#global variables and imports accordingly to used OS
-#TODO: should I move it to constructor?
-from platform import system
-osname = system()
-if osname == 'Linux':
-    from mats_atspi_controller import MatsAtspiController
-    controller = MatsAtspiController()
-elif osname == 'Windows':
-    from mats_msaa_controller import MatsMsaaController
-    controller = MatsMsaaController()
-else:
-    raise Exception("Unsupported platform " + osname + ".")
     
 class MatsRunner(object):
     def __init__(self):
