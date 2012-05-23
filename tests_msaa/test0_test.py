2b0698b9f002a033f7c90ea1888cd0554f46ef34
Andrzej Skalski <askalski>
2012-05-23 18:19:37 +0200

    test 0

---
+++ tests_msaa/test0_test.py
@@ -0,0 +1,30 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import sys
+sys.path.append('../')
+
+from mats_runner import MatsRunner, pyshell 
+import unittest
+from time import sleep
+import os
+from mats_runner import winconstants
+
+class A11yTest1(unittest.TestCase):
+    def setUp(self):
+        self.runner = MatsRunner(config_file = '../winconfig.ini', url = 'file://' + os.path.join(os.getcwd(), 'pages', 'test1.html'))
+        self.runner.start()
+        
+    def test_a11y_instantiated(self):
+        
+        self.assertEqual(True,self.runner.instantiate_a11y())
+        self.assertEqual(True, self.runner.is_a11y_instantiated())
+            
+    def tearDown(self):
+        self.runner.stop()
+        pass
+        
+
+if __name__ == '__main__':
+    unittest.main()
